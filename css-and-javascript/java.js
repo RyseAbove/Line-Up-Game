09c19225e20442b828f8c028e7bf1a4f96553354
zachghosoph <zachariah.ghosoph@student.cart.org>
2023-03-09 15:47:51 -0800

    point system

--- css-and-javascript/java.js
+++ css-and-javascript/java.js
@@ -30,46 +30,58 @@ let prearr = [
 
 let turndata = [false, false, false, false, false]
 
+let rollopt;
+
+let points = [0,0];
+
+
 //placement
 
 let color = "red";
 let fill = "#EE5151";
 
 function placefunc(event) {
-    let box = event.target;
-    let col = box.parentElement.parentElement.id;
-    let num = parseFloat(col.replace('c', '')) - 1;
-    for (let i = 7; i >= 0; i--) {
-        if (boolarr[num][i] == 'e') {
-            boolarr[num][i] = color;
-            prefill(num, i, color);
-            let canvas = document.getElementById(`${num + 1}${i + 1}`);
-            let draw = canvas.getContext("2d");
-            let wid = (top.innerWidth / 100);
-            //( canvas.width/2, canvas.height/2, 2*wid, 0, 2 * Math.PI)
-            draw.ellipse(canvas.width / 2, canvas.height / 2, 4 * wid, 2 * wid, 0, 0, Math.PI * 2)
-            draw.stroke();
-            draw.fillStyle = fill;
-            draw.fill();
-            gamecheck.push(color);
+    if (rollopt == true) {
+        return; 
+    }
+    else {
+        let box = event.target;
+        let col = box.parentElement.parentElement.id;
+        let num = parseFloat(col.replace('c', '')) - 1;
+        for (let i = 7; i >= 0; i--) {
+            if (boolarr[num][i] == 'e') {
+                boolarr[num][i] = color;
+                prefill(num, i, color);
+                let canvas = document.getElementById(`${num + 1}${i + 1}`);
+                let draw = canvas.getContext("2d");
+                let wid = (top.innerWidth / 100);
+                //( canvas.width/2, canvas.height/2, 2*wid, 0, 2 * Math.PI)
+                draw.ellipse(canvas.width / 2, canvas.height / 2, 4 * wid, 2 * wid, 0, 0, Math.PI * 2)
+                draw.stroke();
+                draw.fillStyle = fill;
+                draw.fill();
+                gamecheck.push(color);
 
-            rollfuncl2();
-            rollfuncl3();
-            rollfuncl4();
-            rollfuncl5();
-            rollfuncl6();
-            tellfunc();
-            diefunc();
+                cfunc();
+                rollfuncl2();
+                rollfuncl3();
+                rollfuncl4();
+                rollfuncl5();
+                rollfuncl6();
+                tellfunc();
 
-            if (((gamecheck.length + 1) / 2) % 1 == 0) {
-                color = "blue";
-                fill = "#5187EE"
+                if (((gamecheck.length + 1) / 2) % 1 == 0) {
+                    color = "blue";
+                    precolor = 0;
+                    fill = "#5187EE"
+                }
+                else {
+                    color = "red";
+                    precolor = 1;
+                    fill = "#EE5151";
+                }
+                break;
             }
-            else {
-                color = "red";
-                fill = "#EE5151";
-            }
-            break;
         }
     }
 }
@@ -85,13 +97,17 @@ function fillarr() {
         let m = 0;
         prearr[clist[i][m]][clist[i][m + 1]] = clist[i][m + 2];
     }
+    console.log(prearr);
+}
+
+function cfunc() {
+    for (let i = 0; i < 4; i++) {
+        turndata[i] = false;
+    }
 }
 
 //functions for line checks
 function rollfuncl2() {
-    for (i = 0; i < 4; i++) {
-        turndata[i] = false;
-    }
     for (let i = 0; i < (boolarr.length - 1); i++) {//horizontals
         for (let m = 0; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
@@ -108,10 +124,10 @@ function rollfuncl2() {
             }
         }
     }
-    for (let i = 9; i > 0; i--) {//fix
-        for (let m = 0; m < (boolarr[i].length - 1); m++) {
+    for (let i = 0; i < boolarr.length - 1; i++) {//fix
+        for (let m = 1; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
-                if ((boolarr[i][m] == boolarr[i - 1][m + 1]) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i - 1][m + 1] != prearr[i - 1][m + 1]))) {
+                if ((boolarr[i][m] == boolarr[i + 1][m - 1]) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i + 1][m - 1] != prearr[i + 1][m - 1]))) {
                     turndata[0] = true;
                 }
             }
@@ -145,10 +161,10 @@ function rollfuncl3() {
             }
         }
     }
-    for (let i = 9; i > 1; i--) {//fix
-        for (let m = 0; m < (boolarr[i].length - 2); m++) {
+    for (let i = 0; i < (boolarr.length - 2); i++) {//fix
+        for (let m = 2; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
-                if ((boolarr[i][m] == boolarr[i - 1][m + 1]) && (boolarr[i][m] == boolarr[i - 2][m + 2]) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i - 1][m + 1] != prearr[i - 1][m + 1]) || (boolarr[i - 2][m + 2] == prearr[i - 2][m + 2]))) {
+                if (((boolarr[i][m] == boolarr[i + 1][m - 1]) && (boolarr[i][m] == boolarr[i + 2][m - 2])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i + 1][m - 1] != prearr[i + 1][m - 1]) || (boolarr[i + 2][m - 2] != prearr[i + 2][m - 2]))) {
                     turndata[1] = true;
                 }
             }
@@ -182,10 +198,10 @@ function rollfuncl4() {
             }
         }
     }
-    for (let i = 9; i > 2; i--) {//fixz
-        for (let m = 0; m < (boolarr[i].length - 3); m++) {
+    for (let i = 0; i < boolarr.length - 3; i++) {//fix
+        for (let m = 3; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
-                if (((boolarr[i][m] == boolarr[i - 1][m + 1]) && (boolarr[i][m] == boolarr[i - 2][m + 2]) && (boolarr[i][m] == boolarr[i - 3][m + 3])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i - 1][m + 1] != prearr[i - 1][m + 1]) || (boolarr[i - 2][m + 2] != prearr[i - 2][m + 2]) || (boolarr[i - 3][m + 3] != prearr[i - 3][m + 3]))) {
+                if (((boolarr[i][m] == boolarr[i + 1][m - 1]) && (boolarr[i][m] == boolarr[i + 2][m - 2]) && (boolarr[i][m] == boolarr[i + 3][m - 3])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i + 1][m - 1] != prearr[i + 1][m - 1]) || (boolarr[i + 2][m - 2] != prearr[i + 2][m - 2]) || (boolarr[i + 3][m - 3] != prearr[i + 3][m - 3]))) {
                     turndata[2] = true;
                 }
             }
@@ -219,10 +235,10 @@ function rollfuncl5() {
             }
         }
     }
-    for (let i = 9; i > 3; i--) {//fixz
-        for (let m = 0; m < (boolarr[i].length - 4); m++) {
+    for (let i = 0; i < boolarr.length - 4; i++) {//fix
+        for (let m = 4; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
-                if (((boolarr[i][m] == boolarr[i - 1][m + 1]) && (boolarr[i][m] == boolarr[i - 2][m + 2]) && (boolarr[i][m] == boolarr[i - 3][m + 3]) && (boolarr[i][m] == boolarr[i - 4][m + 4])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i - 1][m + 1] != prearr[i - 1][m + 1]) || (boolarr[i - 2][m + 2] != prearr[i - 2][m + 2]) || (boolarr[i - 3][m + 3] != prearr[i - 3][m + 3]) || (boolarr[i - 4][m + 4] != prearr[i - 4][m + 4]))) {
+                if (((boolarr[i][m] == boolarr[i + 1][m - 1]) && (boolarr[i][m] == boolarr[i + 2][m - 2]) && (boolarr[i][m] == boolarr[i + 3][m - 3]) && (boolarr[i][m] == boolarr[i + 4][m - 4])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i + 1][m - 1] != prearr[i + 1][m - 1]) || (boolarr[i + 2][m - 2] != prearr[i + 2][m - 2]) || (boolarr[i + 3][m - 3] != prearr[i + 3][m - 3]) || (boolarr[i + 4][m - 4] != prearr[i + 4][m - 4]))) {
                     turndata[3] = true;
                 }
             }
@@ -256,10 +272,10 @@ function rollfuncl6() {
             }
         }
     }
-    for (let i = 9; i > 4; i--) {//fixz
-        for (let m = 0; m < (boolarr[i].length - 5); m++) {
+    for (let i = 0; i < boolarr.length - 5; i++) {//fix
+        for (let m = 5; m < boolarr[i].length; m++) {
             if (boolarr[i][m] != 'e') {
-                if (((boolarr[i][m] == boolarr[i - 1][m + 1]) && (boolarr[i][m] == boolarr[i - 2][m + 2]) && (boolarr[i][m] == boolarr[i - 3][m + 3]) && (boolarr[i][m] == boolarr[i - 4][m + 4]) && (boolarr[i][m] == boolarr[i - 5][m + 5])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i - 1][m + 1] != prearr[i - 1][m + 1]) || (boolarr[i - 2][m + 2] != prearr[i - 2][m + 2]) || (boolarr[i - 3][m + 3] != prearr[i - 3][m + 3]) || (boolarr[i - 4][m + 4] != prearr[i - 4][m + 4]) || (boolarr[i - 5][m + 5] != prearr[i - 5][m + 5]))) {
+                if (((boolarr[i][m] == boolarr[i + 1][m - 1]) && (boolarr[i][m] == boolarr[i + 2][m - 2]) && (boolarr[i][m] == boolarr[i + 3][m - 3]) && (boolarr[i][m] == boolarr[i + 4][m - 4]) && (boolarr[i][m] == boolarr[i + 5][m - 5])) && ((boolarr[i][m] != prearr[i][m]) || (boolarr[i + 1][m - 1] != prearr[i + 1][m - 1]) || (boolarr[i + 2][m - 2] != prearr[i + 2][m - 2]) || (boolarr[i + 3][m - 3] != prearr[i + 3][m - 3]) || (boolarr[i + 4][m - 4] != prearr[i + 4][m - 4]) || (boolarr[i + 5][m - 5] != prearr[i + 5][m - 5]))) {
                     turndata[4] = true;
                 }
             }
@@ -281,9 +297,10 @@ function tellfunc() {
     for (let i = 0; i < turndata.length; i++) {
         if (turndata[i] == true) {
             document.getElementById("pos").innerHTML = "a roll is possible";
+            rollopt = true;
             break;
         }
-        else{
+        else {
             document.getElementById("pos").innerHTML = "";
         }
     }
@@ -292,31 +309,79 @@ function tellfunc() {
 let dieresult;
 
 function diefunc() {
-    console.log(turndata)
-    let prob;
+    if (rollopt == false){
+        return;
+    }
+    else{
+        let prob;
     for (let i = 0; i < turndata.length; i++) {
         if (turndata[i] == true) {
             prob = i;
         }
     }
-
     if (prob == 0) {
-        console.log("roll for a 1");
+        roller();
+        if (dieresult == 0) {
+            addpoint();
+        }
+        else {
+            document.getElementById("pos").innerHTML = "no points gained"
+        }
+        rollopt = false;
     }
     else if (prob == 1) {
-        console.log("roll for a 1,2");
+        roller();
+        if (dieresult == (0 || 1)) {
+            addpoint();
+        }
+        else {
+            document.getElementById("pos").innerHTML = "no points gained"
+        }
+        rollopt = false;
     }
     else if (prob == 2) {
-        console.log("roll for a 1,2,3");
+        roller();
+        if (dieresult == (0 || 1 || 2)) {
+            addpoint();
+        }
+        else {
+            document.getElementById("pos").innerHTML = "no points gained"
+        }
+        rollopt = false;
     }
     else if (prob == 3) {
-        console.log("roll for a 1,2,3,4");
+        roller();
+        if (dieresult == (0 || 1 || 2 || 3)) {
+            addpoint();
+        }
+        else {
+            document.getElementById("pos").innerHTML = "no points gained"
+        }
+        rollopt = false;
     }
     else if (prob == 4) {
-        console.log("roll for a 1,2,3,4,5");
+        roller();
+        if (dieresult == (0 || 1 || 2 || 3 || 4)) {
+            addpoint();
+        }
+        else {
+            document.getElementById("pos").innerHTML = "no points gained"
+        }
+        rollopt = false;
     }
+    }
+    
 }
 
-function roller(){
-    dieresult = Math.floor(Math.random())*6;
+function addpoint(){
+    points[precolor] += 1;
+    document.getElementById("point-red").innerHTML = points[0];
+    document.getElementById("point-blue").innerHTML = points[1];
+}
+
+
+
+function roller() {
+    dieresult = Math.floor(Math.random()*6);
+    console.log(dieresult)
 }
