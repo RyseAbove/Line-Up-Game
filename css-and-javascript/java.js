197ff388624fcce4cd5f7f26b4b986389e723c78
Noah L <116219668+RyseAbove@users.noreply.github.com>
2023-03-23 09:49:35 -0700

    ai opponent (breaks chrome tab)

--- css-and-javascript/java.js
+++ css-and-javascript/java.js
@@ -134,107 +134,107 @@ for (let i = 0; i < canvelem.length; i++) {
 
 // ai array
 
-// let aiarray = [[0], [0], [0], [0], [0]]
+let aiarray = [[0], [0], [0], [0], [0]];
 
-// function aiPlace() {
-//     if (color == 'blue') {
-//         for (let i = 0; i < (boolarr.length - 1); i++) {
-//             for (let m = 0; m < boolarr[i].length; m++) {
-//                 if (boolarr[i][m] == 'e') {
-//                     switch (true) {
-//                         case (boolarr[i + 1][m]):
-//                             aiarray[0].push[i, m];
-//                         case (boolarr[i + 1][m + 1]):
-//                             aiarray[0].push[i, m];
-//                         case (boolarr[i + 1][m - 1]):
-//                             aiarray[0].push[i, m];
-//                         case (boolarr[i][m + 1]):
-//                             aiarray[0].push[i, m];
-//                         case (boolarr[i + 2][m]):
-//                             aiarray[1].push[i, m];
-//                         case (boolarr[i + 2][m + 2]):
-//                             aiarray[1].push[i, m];
-//                         case (boolarr[i + 2][m - 2]):
-//                             aiarray[1].push[i, m];
-//                         case (boolarr[i][m + 2]):
-//                             aiarray[1].push[i, m];
-//                         case (boolarr[i + 3][m]):
-//                             aiarray[2].push[i, m];
-//                         case (boolarr[i + 3][m + 3]):
-//                             aiarray[2].push[i, m];
-//                         case (boolarr[i + 3][m - 3]):
-//                             aiarray[2].push[i, m];
-//                         case (boolarr[i][m + 3]):
-//                             aiarray[2].push[i, m];
-//                         case (boolarr[i + 4][m]):
-//                             aiarray[3].push[i, m];
-//                         case (boolarr[i + 4][m + 4]):
-//                             aiarray[3].push[i, m];
-//                         case (boolarr[i + 4][m - 4]):
-//                             aiarray[3].push[i, m];
-//                         case (boolarr[i][m + 4]):
-//                             aiarray[3].push[i, m];
-//                         case (boolarr[i + 5][m]):
-//                             aiarray[4].push[i, m];
-//                         case (boolarr[i + 5][m + 1]):
-//                             aiarray[4].push[i, m];
-//                         case (boolarr[i + 5][m - 5]):
-//                             aiarray[4].push[i, m];
-//                         case (boolarr[i][m + 5]):
-//                             aiarray[4].push[i, m];
-//                         case (boolarr[i + 6][m]):
-//                             aiarray[5].push[i, m];
-//                         case (boolarr[i + 6][m + 6]):
-//                             aiarray[5].push[i, m];
-//                         case (boolarr[i + 6][m - 6]):
-//                             aiarray[5].push[i, m];
-//                         case (boolarr[i][m + 6]):
-//                             aiarray[5].push[i, m];
-//                     }
-//                 }
-//             }}}
-//         } for (let i = 0; i < aiarray.length; i++) {
-//             if (aiarray[i + 1] = 0) {
-//                 let aiChoice = (Math.floor(Math.random() * aiarray[i].length));
-//                 let aiColumn = aiarray[i][aiChoice][0];
-//                 let aiRow = aiarray[i][aiChoice][1];
-//                 let box = document.getElementById(`${aiColumn + 1}${aiRow + 1}`);
-//                 let col = box.parentElement.parentElement.id;
-//                 let num = parseFloat(col.replace('c', '')) - 1;
-//                 if (boolarr[num][i] == 'e') {
-//                     boolarr[num][i] = color;
-//                     prefill(num, i, color);
-//                     let canvas = document.getElementById(`${num + 1}${i + 1}`);
-//                     let draw = canvas.getContext("2d");
-//                     let wid = (top.innerWidth / 100);
-//                     draw.ellipse(canvas.width / 2, canvas.height / 2, 4 * wid, 2 * wid, 0, 0, Math.PI * 2);
-//                     draw.stroke();
-//                     draw.fillStyle = fill;
-//                     draw.fill();
-//                     gamecheck.push(color);
+function aiPlace() {
+    if (color == 'blue') {
+        for (let i = 0; i < (boolarr.length - 1); i++) {
+            for (let m = 0; m < boolarr[i].length; m++) {
+                if (boolarr[i][m] == 'e') {
+                    switch (true) {
+                        case (boolarr[i + 1][m]):
+                            aiarray[0].push[i, m];
+                        case (boolarr[i + 1][m + 1]):
+                            aiarray[0].push[i, m];
+                        case (boolarr[i + 1][m - 1]):
+                            aiarray[0].push[i, m];
+                        case (boolarr[i][m + 1]):
+                            aiarray[0].push[i, m];
+                        case (boolarr[i + 2][m]):
+                            aiarray[1].push[i, m];
+                        case (boolarr[i + 2][m + 2]):
+                            aiarray[1].push[i, m];
+                        case (boolarr[i + 2][m - 2]):
+                            aiarray[1].push[i, m];
+                        case (boolarr[i][m + 2]):
+                            aiarray[1].push[i, m];
+                        case (boolarr[i + 3][m]):
+                            aiarray[2].push[i, m];
+                        case (boolarr[i + 3][m + 3]):
+                            aiarray[2].push[i, m];
+                        case (boolarr[i + 3][m - 3]):
+                            aiarray[2].push[i, m];
+                        case (boolarr[i][m + 3]):
+                            aiarray[2].push[i, m];
+                        case (boolarr[i + 4][m]):
+                            aiarray[3].push[i, m];
+                        case (boolarr[i + 4][m + 4]):
+                            aiarray[3].push[i, m];
+                        case (boolarr[i + 4][m - 4]):
+                            aiarray[3].push[i, m];
+                        case (boolarr[i][m + 4]):
+                            aiarray[3].push[i, m];
+                        case (boolarr[i + 5][m]):
+                            aiarray[4].push[i, m];
+                        case (boolarr[i + 5][m + 1]):
+                            aiarray[4].push[i, m];
+                        case (boolarr[i + 5][m - 5]):
+                            aiarray[4].push[i, m];
+                        case (boolarr[i][m + 5]):
+                            aiarray[4].push[i, m];
+                        case (boolarr[i + 6][m]):
+                            aiarray[5].push[i, m];
+                        case (boolarr[i + 6][m + 6]):
+                            aiarray[5].push[i, m];
+                        case (boolarr[i + 6][m - 6]):
+                            aiarray[5].push[i, m];
+                        case (boolarr[i][m + 6]):
+                            aiarray[5].push[i, m];
+                    }
+                }
+            }}}
+        } for (let i = 0; i < aiarray.length; i++) {
+            if (aiarray[i + 1] = 0) {
+                let aiChoice = (Math.floor(Math.random() * aiarray[i].length));
+                let aiColumn = aiarray[i][aiChoice][0];
+                let aiRow = aiarray[i][aiChoice][1];
+                let box = document.getElementById(`${aiColumn + 1}${aiRow + 1}`);
+                let col = box.parentElement.parentElement.id;
+                let num = parseFloat(col.replace('c', '')) - 1;
+                if (boolarr[num][i] == 'e') {
+                    boolarr[num][i] = color;
+                    prefill(num, i, color);
+                    let canvas = document.getElementById(`${num + 1}${i + 1}`);
+                    let draw = canvas.getContext("2d");
+                    let wid = (top.innerWidth / 100);
+                    draw.ellipse(canvas.width / 2, canvas.height / 2, 4 * wid, 2 * wid, 0, 0, Math.PI * 2);
+                    draw.stroke();
+                    draw.fillStyle = fill;
+                    draw.fill();
+                    gamecheck.push(color);
 
-//                     cfunc();
-//                     rollfuncl2();
-//                     rollfuncl3();
-//                     rollfuncl4();
-//                     rollfuncl5();
-//                     rollfuncl6();
-//                     tellfunc();
+                    cfunc();
+                    rollfuncl2();
+                    rollfuncl3();
+                    rollfuncl4();
+                    rollfuncl5();
+                    rollfuncl6();
+                    tellfunc();
 
-//                     if (((gamecheck.length + 1) / 2) % 1 == 0) {
-//                         color = "blue";
-//                         precolor = 0;
-//                         fill = "#5187EE"
-//                     }
-//                     else {
-//                         color = "red";
-//                         precolor = 1;
-//                         fill = "#EE5151";
-//                     }
-//                     break;
-//                 }
-//             }
-//         }
+                    if (((gamecheck.length + 1) / 2) % 1 == 0) {
+                        color = "blue";
+                        precolor = 0;
+                        fill = "#5187EE"
+                    }
+                    else {
+                        color = "red";
+                        precolor = 1;
+                        fill = "#EE5151";
+                    }
+                    break;
+                }
+            }
+        }
 
 /*filling prearr*/
 
